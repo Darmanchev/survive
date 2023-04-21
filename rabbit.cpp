28db25b937e83e25e4a6024cd2a1c87e50119522
Darmanchev <darmanchev.evgeni@gmail.com>
2023-04-21 16:46:29 +0300

    rabbit

---
+++ rabbit.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+   // Sorry, I did not understand all the terms of the assignment.
+   // I can suggest that if the sides of the garden are not an even number,
+   // we divide it by 2 and round it down. These are our jumps
+
+   return 0;
+}
